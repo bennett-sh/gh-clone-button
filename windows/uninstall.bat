04c5a2ae6eeef63e980ba891d52192a6f25cd4c5
bennett-sh <110846042+bennett-sh@users.noreply.github.com>
2022-09-10 22:14:12 +0200

    Add comment on confusing line

--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -1,5 +1,6 @@
 @echo off
 
+rem UAC Elevation
 NET FILE > NUL 2>&1 || POWERSHELL -ex Unrestricted -Command "Start-Process -Verb RunAs -FilePath '%ComSpec%' -ArgumentList '/c \"%~fnx0\" %*'" && EXIT /b
 
 echo Removing URL protocol.
