037dd11a540952d89e68e39018dc3c46dbc0326c
Bennett <110846042+bennett-sh@users.noreply.github.com>
2024-10-15 02:37:03 +0200

    Update for new GitHub updates

--- clone-button.user.js
+++ clone-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clone Button
 // @namespace    https://github.com/bennett-sh
-// @version      1.0
+// @version      1.1
 // @description  Adds a 'Clone'-button to GitHub
 // @author       BSh
 // @match        https://github.com/*
@@ -14,10 +14,18 @@ const $ = s => document.querySelector(s)
 function update(path) {
     if(!/\/[a-z0-9\-\_]*\/[a-z0-9\-\_]*/i.test(path)) return
 
-    if($(".file-navigation > .clone-button")) return
+    if($(".jxTzTd > .clone-button")) return
 
-    $(".file-navigation")?.insertAdjacentHTML('beforeend', `<a class="btn btn-primary ml-2 d-none d-md-block clone-button" data-hotkey="c" href="git-clone://https://github.com${path}">Clone</a>`)
-    $("[data-action=\"toggle:get-repo#onDetailsToggle\"] > .btn-primary")?.classList.remove('btn-primary')
+    $(".jxTzTd")?.insertAdjacentHTML('beforeend', `<a class="btn btn-primary ml-2 d-none d-md-block clone-button" data-hotkey="c" href="git-clone://https://github.com${path}">Clone</a>`)
+
+    const style = document.createElement('style')
+    style.textContent = `
+    .jxTzTd button:has(+.izFOf) {
+        color: var(--button-default-fgColor-rest,var(--color-btn-text,#c9d1d9));
+        background-color: var(--button-default-bgColor-rest,var(--color-btn-bg,#21262d));
+    }
+    `
+    document.body.append(style)
 }
 
 
